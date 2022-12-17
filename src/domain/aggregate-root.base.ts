d0cb91bc72a778b14f14c92b60707c67a54d7910
Nguyen Chi Cuong <nguyenchicuong31998@gmail.com>
2022-12-17 10:27:19 +0700

    t

--- src/domain/aggregate-root.base.ts
+++ src/domain/aggregate-root.base.ts
@@ -1,19 +1,3 @@
 import { Entity } from "../domain/entity.base";
 
-export abstract class AggregateRoot<T> extends Entity<T> {
-    private _domainEvents: DomainEvent = [];
-    get domainEvents : DomainEvent[] {
-        return this._domainEvents;
-    }
-
-    protected addEvents(domainEvents : DomainEvent) : void{
-        this._domainEvents.push(domainEvents);
-        DomainEvents.prepareForPublish(this);
-    }
-
-    
-
-    protected clearEvents(){
-        this._domainEvents = [];
-    }
-}
+export abstract class AggregateRoot<T> extends Entity<T> {}
