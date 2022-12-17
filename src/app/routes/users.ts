f814c5d8aec28d3612d12d39bf135bf0a8461396
Nguyen Chi Cuong <nguyenchicuong31998@gmail.com>
2022-12-17 11:42:27 +0700

    tdsadadsadsa

--- src/app/routes/users.ts
+++ src/app/routes/users.ts
@@ -7,7 +7,7 @@ router.get(`/`, async (req: Request, res: Response) => {
         const query = { ...req.query };
         res.status(200).json({
             data: {
-                name: "ok  nek sss cuong s",
+                name: "het roi nguoi oi",
             },
         });
     } catch (err) {
